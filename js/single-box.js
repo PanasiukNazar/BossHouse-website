8d784957b92f2f4cc6f1bf577d591d900d74989c
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-07-17 19:05:42 +0300

    WIP

--- js/single-box.js
+++ js/single-box.js
@@ -49,47 +49,83 @@ const boxes = info.map((singleBox) => {
         )
         .join('');
 
-    const isAvailable = `singleBox.available
-        ? <p style="color: '#45BA89'">'В Наявності'</p>
-        : <p style="color: '#FF0000'">'Немає В Наявності'</p>`;
+    const isAvailable = singleBox.available
+        ? `<p style="color: #45BA89;">В Наявності</p>`
+        : `<p style="color: #d20000;">Немає В Наявності</p>`;
+
     return `
-    <div class="col-md-6">
-        <div class="images__slider">
-            <div class="preview__image">
-                ${images}
-            </div>
-        </div>
-        <div class="delivery__content">
-            <div class="price">
-                ${singleBox.price} <span class='currency'>грн</span>
-            </div>
-            <div class="memo">
-                <img src="/assets/images/icons/delivery-icon.png" alt="" />
-                <div class="delivery__info">
-                    <h4>Доставка по Україні</h4>
-                    <span>за тарифами перевізника в межах 200 - 400 грн</span>
+        <div class="col-md-6">
+            <div class="images__slider">
+                <div class="preview__image">
+                    ${images}
                 </div>
             </div>
-        </div>
-        <div class="box__description">
-            <h5>Опис</h5>
-            ${description}
-        </div>
-    </div>
-
-
-    <div class="col-md-6">
-        <div class="single__header">
-            <div class="single__type">
-                <p>${singleBox.type}</p>
-                <h3>${singleBox.name}</h3>
+            <div class="delivery__content">
+                <div class="price">
+                    ${singleBox.price} <span class='currency'>грн</span>
+                </div>
+                <div class="memo">
+                    <img src="/assets/images/icons/delivery-icon.png" alt="" />
+                    <div class="delivery__info">
+                        <h4>Доставка по Україні</h4>
+                        <span>за тарифами перевізника в межах 200 - 400 грн</span>
+                    </div>
+                </div>
             </div>
-            <div>
-                ${isAvailable}
+            <div class="box__description">
+                <h5>Опис</h5>
+                ${description}
             </div>
         </div>
-    </div>
-`;
+    
+        <div class="col-md-6">
+            <div class="single__header">
+                <div class="single__type">
+                    <span>${singleBox.type}</span>
+                    <h3>${singleBox.name}</h3>
+                </div>
+                <div>
+                    ${isAvailable}
+                </div>
+            </div>
+    
+            <div class="single__body">
+                <div class="box__size">
+                    <span>Розмір:</span>
+                    <select>
+                        <option>${singleBox.size.small.join(' ')}</option>
+                        <option>${singleBox.size.big.join(' ')}</option>
+                    </select>
+                </div>
+                <div class="box__material">
+                    <span>Матеріал:</span>
+                    <p>Дерево, рубероїд, пінополістирол, фанера/0513</p>
+                </div>
+                <div class="box__color">
+                    <span>Колір:</span>
+                    <div class="color__options">
+                        <input type="checkbox" id="color1"/>
+                        <label for="color1">Не фарбована</label>
+    
+                        <input type="checkbox" id="color2"/>
+                        <label for="color2">Світлий Дуб</label>
+    
+                        <input type="checkbox" id="color3"/>
+                        <label for="color3">Темний Дуб</label>
+    
+                        <input type="checkbox" id="color4"/>
+                        <label for="color4">Сосна</label>
+                    </div>
+                </div>
+            </div>
+            <div class="single__footer">
+                <div class="actions">
+                    <!-- Add any action buttons or links here -->
+                </div>
+                <a class="more__info">Більше інформації <i class="bi bi-heart-fill"></i></a>
+            </div>
+        </div>
+    `;
 });
 
 document.querySelector('#box-info').innerHTML = boxes.join('\n');
